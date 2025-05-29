78b9324fac7723539bfd995c807c8057f069ed51
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2025-05-29 11:10:15 +0300

    Add project files.

---
+++ EmployeesManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+         public DbSet<Employee> Clients { get; set; }
+    }
+}
